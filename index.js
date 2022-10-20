091f32dbe6eb790430a066ecce417c299b40f4be
escuela@davinci.edu.ar <63879459+sergiomedinaio@users.noreply.github.com>
2022-10-20 16:47:09 -0300

    feat: add list of people

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
+      peopleList: [],
       message: 'Hello Vue!'
+    },
+    methods: {
+      async getFromRemote(){
+        let response = await fetch("https://randomuser.me/api/?results=50")
+        if(response.ok) { //200 y 299
+          const {results} = await response.json();
+          this.peopleList = results;
+        }
+      }
     }
   })
   
