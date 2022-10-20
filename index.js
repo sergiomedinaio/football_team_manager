3d67e8731cc0ea6e94e03b053d7a324276eba1d6
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-10-20 17:25:10 -0300

    feat: select person from list in modal

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ var app = new Vue({
     el: '#app',
     data: {
       peopleList: [],
+      showModal: false,
+      peopleSelected: [],
       message: 'Hello Vue!'
     },
     methods: {
+      selectPerson (person, index) {
+        //person.gender = "male";
+        this.peopleSelected.push(person);
+        this.peopleList.splice(index,1);
+        this.showModal = false;
+      },
       async getFromRemote(){
         let response = await fetch("https://randomuser.me/api/?results=50")
         if(response.ok) { //200 y 299
           const {results} = await response.json();
-          this.peopleList = results;
+          this.peopleList = results.filter(person => person.dob.age >= 18);
+          this.showModal = true;
         }
       }
     }
